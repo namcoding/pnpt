1705183d2ce99e13f0ace17f45eb135a3a333dbd
nguyễn thanh nam <nammysql@gmail.com>
2015-02-27 20:08:39 +0700

    Update and rename appication to appication/model/mget.php

---
+++ appication/model/mget.php
@@ -0,0 +1,11 @@
+<?php 
+	class Mget extends Pnpt_model{
+			function nam(){
+				return "model da thuc hien<br>";
+			}
+			function thuy(){
+				return "fffffffffffff";
+				//$this->load_view('')
+			}
+		}
+?>
