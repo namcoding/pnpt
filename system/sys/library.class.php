cc1325a217d59a181b87b732f0482e5d6e03b8f8
nguyễn thanh nam <nammysql@gmail.com>
2015-02-27 20:29:49 +0700

    Update and rename system/sys to system/sys/library.class.php

---
+++ system/sys/library.class.php
@@ -0,0 +1,30 @@
+<?php 
+	class Pnpt_library{
+		private static $instance;
+		protected $load;
+	    public function __construct(){
+			self::$instance =& $this;
+		}
+		public static function &get_instance(){
+			return self::$instance;
+		}
+		function get_method($method){
+			$obj= new $this;
+			//var_dump($obj);
+			return function() use($obj,$method){
+			$args = func_get_args();
+			return call_user_func_array(array($obj, $method), $args);          
+			};
+		}
+		public function load(){
+			if(!file_exists(SYSPATH."load_class.php")){
+				header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+				echo 'Your appication folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+				exit(3); // EXIT_CONFIG
+			}
+			require_once(SYSPATH."load_class.php");
+			return new Auto_load;
+		}
+	}
+	
+?>
